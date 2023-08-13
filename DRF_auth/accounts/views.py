fedbf957719b3b1224f49e5c54822588b44e1e78
Artsiom <prohorenkoartem111@gmail.com>
2023-08-13 12:29:12 +0200

    RegisterAPI

--- DRF_auth/accounts/views.py
+++ DRF_auth/accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class RegisterAPI(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = UserSerializer()
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status': 200,
+                    'message': 'registration successfully check email',
+                    'data': serializer.data,
+
+                })
+            return Response({
+                'status': 400,
+                'message': 'something went wrong',
+                'data': serializer.errors
+            })
+        except Exception as e:
+            print(e)
