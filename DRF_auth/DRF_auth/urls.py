838bcee796be3c93fdc01b47d717db7b37a4c1e5
Artsiom <prohorenkoartem111@gmail.com>
2023-08-13 16:37:00 +0200

    stable otp version

--- DRF_auth/DRF_auth/urls.py
+++ DRF_auth/DRF_auth/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from accounts.views import RegisterAPI
+from accounts.views import RegisterAPI, VerifyOTP
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view()),
     path('admin/', admin.site.urls),
+    path('verify/', VerifyOTP.as_view())
 ]
