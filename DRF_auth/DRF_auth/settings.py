99aa92ecaa6e5e059f63d21b502f378916fd0025
Artsiom <prohorenkoartem111@gmail.com>
2023-08-13 13:06:14 +0200

    add new files

--- DRF_auth/DRF_auth/settings.py
+++ DRF_auth/DRF_auth/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from .secret import secret_password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'prohorenkoartem111@gmail.com'
+EMAIL_HOST_PASSWORD = secret_password
+EMAIL_PORT = 587
---
+++ DRF_auth/accounts/emails.py
@@ -0,0 +1,11 @@
+from django.core.mail import send_mail
+import random
+from django.conf import settings
+
+
+def send_otp_via_email(email):
+    subject = 'Your account verification email'
+    otp = random.randint(1000, 9999)
+    message = f'Your OTP is {otp}'
+    email_from = settings.EMAIL_HOST
+    send_mail(subject, message, email_from, [email])
